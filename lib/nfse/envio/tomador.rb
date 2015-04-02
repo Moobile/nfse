5d2739012a376288f681d590c9cd3db5124e7286
Estevão Andrade <estevaomoraes1@hotmail.com>
2015-04-02 16:09:56 -0300

    [Fix] Show tomador data

--- lib/nfse/envio/tomador.rb
+++ lib/nfse/envio/tomador.rb
@@ -4,19 +4,19 @@ module Nfse
       include Virtus
 
       attribute :inscricao_municipal, String
-      attribute :cnpj, String, default: :default_cnpj
-      attribute :razao_social, String, default: :default_razao_social
+      attribute :cnpj, String
+      attribute :razao_social, String
       attribute :doc_estrangeiro, String
-      attribute :tipo_logradouro, String, default: :default_dash
-      attribute :logradouro, String, default: :default_dash
-      attribute :num_endereco, String, default: :default_num_endereco
+      attribute :tipo_logradouro, String
+      attribute :logradouro, String
+      attribute :num_endereco, String
       attribute :complemento_endereco, String
-      attribute :tipo_bairro, String, default: :default_dash
-      attribute :bairro, String, default: :default_dash
+      attribute :tipo_bairro, String
+      attribute :bairro, String
       attribute :cod_cidade, String
       attribute :cidade, String
-      attribute :cep, String, default: :default_cep
-      attribute :email, String, default: :default_dash
+      attribute :cep, String
+      attribute :email, String
       attribute :ddd, String
       attribute :telefone, String
 
@@ -24,11 +24,15 @@ module Nfse
       attribute :cpf, String
       attribute :uf, String
 
-      def tem_endereco?
+
+      attribute :tem_endereco, String, default: :default_tem_endereco
+      attribute :tem_contato, String, default: :default_tem_contato
+
+      def default_tem_endereco
         [@logradouro, @num_endereco, @complemento_endereco, @bairro, @cod_cidade, @uf, @cep].uniq != [nil]
       end
 
-      def tem_contato?
+      def default_tem_contato
         [@telefone, @email].uniq != [nil]
       end
 
@@ -41,26 +45,7 @@ module Nfse
         cep.ljust(8, '0') unless cep.nil?
       end
 
-      # Attributes default values
-      def default_cnpj
-        '77777777777' if Nfse::Base.prefeitura == :campinas
-      end
-
-      def default_razao_social
-        'Consumidor' if Nfse::Base.prefeitura == :campinas
-      end
-
-      def default_dash
-        '-' if Nfse::Base.prefeitura == :campinas
-      end
-
-      def default_num_endereco
-        '0' if Nfse::Base.prefeitura == :campinas
-      end
-
-      def default_cep
-        '00000000' if Nfse::Base.prefeitura == :campinas
-      end
+     
     end
   end
 end
